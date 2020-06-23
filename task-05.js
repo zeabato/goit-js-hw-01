634b367254b555d738fc1c385840899571ffb08e
Artem <39087318+zeabato@users.noreply.github.com>
2020-06-23 12:18:07 +0300

    hw final

--- task-05.js
+++ task-05.js
@@ -1,27 +1,32 @@
 'use strict';
 
-let priceCountry = {'китай': 100, 'чілі': 250, 'австралия': 170, 'індія': 80, 'ямайка': 120}
+// let priceCountry = {'китай': 100, 'чілі': 250, 'австралия': 170, 'індія': 80, 'ямайка': 120}
 
 let nameCountry = prompt('Куди доставити?');
 
 nameCountry = nameCountry.toLowerCase();
-
+let priceCountry = null;
 
 switch (nameCountry){
   case 'китай':
-    alert(`Доставка в ${nameCountry} буде коштувати ${priceCountry.китай} кредитів`);
+    priceCountry = 100;
+    alert(`Доставка в ${nameCountry} буде коштувати ${priceCountry} кредитів`);
     break;
   case 'чілі':
-    alert(`Доставка в ${nameCountry} буде коштувати ${priceCountry.чілі} кредитів`);
+    priceCountry = 250;
+    alert(`Доставка в ${nameCountry} буде коштувати ${priceCountry} кредитів`);
     break;
   case 'австралія':
-    alert(`Доставка в ${nameCountry} буде коштувати ${priceCountry.австралия} кредитів`);
+    priceCountry = 170;
+    alert(`Доставка в ${nameCountry} буде коштувати ${priceCountry} кредитів`);
     break;
   case 'індія':
-    alert(`Доставка в ${nameCountry} буде коштувати ${priceCountry.індія} кредитів`);
+    priceCountry = 80;
+    alert(`Доставка в ${nameCountry} буде коштувати ${priceCountry} кредитів`);
     break;
   case 'ямайка':
-    alert(`Доставка в ${nameCountry} буде коштувати ${priceCountry.ямайка} кредитів`);
+    priceCountry = 120;
+    alert(`Доставка в ${nameCountry} буде коштувати ${priceCountry} кредитів`);
     break;
   default:
     alert('У вашій країні доставка недоступна');
